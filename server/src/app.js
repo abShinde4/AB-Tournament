86bd9d8f84ad9c3f29bfaddf1b9ab3a00f6a7331
ab sigma student <ab.shinde1910@email.com>
2026-05-08 00:07:57 +0530

    Added localhost to CORS

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,9 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ["https://ab-tournament.vercel.app",
+  origin: [
+    "http://localhost:5173",
+    "https://ab-tournament.vercel.app",
     "https://ab-tournament-git-main-abshinde4s-projects.vercel.app"
   ],
   
